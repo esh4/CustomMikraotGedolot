2047fae2fc0f4f2ac5227d23119b5e0378e7cd92
Eshel Sinclair <eshel.sinclair@gmail.com>
2020-08-25 00:24:56 +0300

    added build

---
+++ functions/src/Web_ui/build/precache-manifest.a07457b97b2c6ed631e69a4d00adffb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77da90dd0757df72619dc67d6867fb6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fab41eff0ca67e453cd",
+    "url": "/static/css/main.a8d6f538.chunk.css"
+  },
+  {
+    "revision": "918a844cd4116beeffdb",
+    "url": "/static/js/2.61a56671.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.61a56671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fab41eff0ca67e453cd",
+    "url": "/static/js/main.12aafe32.chunk.js"
+  },
+  {
+    "revision": "1ab6386d2e9c82ad4a76",
+    "url": "/static/js/runtime-main.5c888476.js"
+  }
+]);
